d80a449c31182cdd79a1e748f941ce8639df03a2
Scott Ranger <scott@soccerhubb.com>
2015-02-13 11:22:52 +0000

    Small refactor

--- src/TwitterApi.js
+++ src/TwitterApi.js
@@ -4,13 +4,15 @@ var APP_KEY = Symbol();
 var APP_SECRET = Symbol();
 var INTERNAL_API = Symbol();
 
+//todo: urls into seperate json
+
 export default class TwitterApi {
 	constructor(app_key, app_secret) {
 		if (app_key === undefined) throw "Error: TwitterApi - Twitter app key must be provided";
 		if (app_secret === undefined) throw "Error: TwitterApi - Twitter app secret must be provided";
 		if (typeof(app_key) != "string" || !app_key) throw "Error: TwitterApi - Twitter app key must be a valid string";
 		if (typeof(app_secret) != "string" || !app_secret) throw "Error: TwitterApi - Twitter app secret must be a valid string";
-	
+
 		this[APP_KEY] = app_key;
 		this[APP_SECRET] = app_secret;
 		this[INTERNAL_API] = new Purest({
@@ -21,38 +23,42 @@ export default class TwitterApi {
 	}
 
 	PostTweet(data) {
-		return new Promise((res, rej) => {
-			return this[INTERNAL_API].post(
-				'statuses/update',
-				data,
-				(error, result) => error ? rej(error) : res(result)
-			);
-		});
+		return POST('statuses/update', data);
 	}
 
 	SearchUsers(data) {
-		return new Promise((res, rej) => {
-			return this[INTERNAL_API].get(
-				'users/search',
-				data,
-				(error, result) => error ? rej(error) : res(result)
-			);
-		});
+		return GET('users/search', data);
 	}
 
 	GetFollowers(data) {
-		return new Promise((res, rej) => {
-			return this[INTERNAL_API].get(
-				'followers/ids',
-				data,
-				(error, result) => error ? rej(error) : res(result)
-			);
-		});
+		return GET('followers/ids', data);
+	}
+
+	FollowUser(data) {
+		return POST('friendships/create', data);
+	}
+
+	UnfollowUser(data) {
+		return POST('friendships/destroy', data);
 	}
 }
 
- // friendships/destroy
- // friendships/create
- // followers/ids
+function GET(url, data) {
+	return new Promise((res, rej) => {
+		return this[INTERNAL_API].get(
+			url,
+			data,
+			(error, result) => error ? rej(error) : res(result)
+		);
+	});
+}
 
- //todo: urls into seperate json
+function POST(url, data) {
+	return new Promise((res, rej) => {
+		return this[INTERNAL_API].post(
+			url,
+			data,
+			(error, result) => error ? rej(error) : res(result)
+		);
+	});
+}
